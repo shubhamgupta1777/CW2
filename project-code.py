2539b3870f18a965edb46e97a7a820576b5c7572
Milad Roshanaei Zadeh <125696083+miladrzz@users.noreply.github.com>
2023-12-07 04:14:17 +0000

    Add files via upload
    Completed version

--- project-code.py
+++ project-code.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
 import json
 import matplotlib.pyplot as plt
+import os
 from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk, Image
@@ -7,8 +9,34 @@ from tkinter import filedialog
 from tkinter import messagebox
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+def format_json():
 
-data = []
+    input_file_path = './sample_600k.json'
+
+    # Initialize variables
+    line_count = 1
+    output_file = open(f'output_file.json', 'w')
+    # Read input file and write to different files
+    with open(input_file_path, 'r') as input_file:
+        lines = input_file.readlines()
+        print(len(lines))
+        output_file.write("{\"logs\":[\n")
+        l = ''
+        for i,line in enumerate(lines):
+            l = line 
+            if i != len(lines)-1:
+                if '}' in line: 
+                    output_file.write(l.replace('}',"},"))
+            else:
+                output_file.write(l.replace('}',"}]}\n"))
+
+            line_count += 1
+            
+    # Close the last output file
+    output_file.close()
+format_json()
+f = open('output_file.json')
+data = json.load(f)
 main = None
 window = None
 flag = 0
@@ -27,18 +55,15 @@ def get_countries():
     #get number of countries.
     #for giving the number of each country to it we need a dictionary.
     #first element of this dictionary is the name of country and the second one is the number of repeatitions.
-    countries_dict = {}
-    for item in countries_set:
-        countries_dict[item]=countries.count(item)
     countries_weights = []
     #to create histograms we need to export the values from dictionary and give them to hist().
     for item in countries_set:
         countries_weights.append(countries.count(item))
     result = [countries,countries_set,countries_weights]
-    show_countries_hist(result[0],result[1],result[2])
+    show_countries_hist(result[2],result[2])
         
-def show_countries_hist(countries,countries_set,countries_weights):
-    plt.hist(countries_set,weights=countries_weights)
+def show_countries_hist(countries_set,countries_weights):
+    plt.hist(countries_set, len(countries_set) ,ec="red",weights=countries_weights)
     plt.ylabel('Repeatitions')
     plt.xlabel('Countries')
     plt.title("Countries Repeatition Histogram")
@@ -54,30 +79,23 @@ def get_browsers():
     #get the browsers and identify the most popular ones.
     browsers=[]
     for log in data['logs']:
-        browsers.append(log['visitor_useragent'])
-    #change the whole line to the browser name only. apparently all the browser names are followed by a '/' character.
-    #let's slice our strings!
-    for item in browsers:
-        item = str(item)
-        point = item.find('/')
-        #slice the string to the '/' index.
-        slice = item[:point:]
-        browsers = list(map(lambda x: x.replace(item,slice),browsers))
+        string = str(log['visitor_useragent'])
+        slash = string.find('/')
+        string = string[:slash]
+        browsers.append(string)
 
     #create a histogram for the browsers.
     browsers_set = set(browsers)
-    browsers_dict = {}
-    for item in browsers_set:
-        browsers_dict[item] = browsers.count(item)
     browsers_weights = []
     #set the histogram
     for item in browsers_set:
         browsers_weights.append(browsers.count(item))
+        
     result = [browsers,browsers_set,browsers_weights]
-    show_browsers_hist(result[0],result[1],result[2])
+    show_browsers_hist(result[1],result[2])
 
-def show_browsers_hist(browsers,browsers_set,browsers_weights):
-    plt.hist(browsers_set,weights=browsers_weights)
+def show_browsers_hist(browsers_set,browsers_weights):
+    plt.hist(browsers_set, len(browsers_set) ,ec="red",weights=browsers_weights)
     plt.ylabel('Repeatitions')
     plt.xlabel('Browsers')
     plt.title("Browsers Repeatition Histogram")
@@ -92,29 +110,24 @@ def user_timeSpent():
     global data
     #readers time spent.
     #first I will add the users in a set.
-    users_set = []
+    users = []
+    users_time={}
     for log in data['logs']:
-        users_set.append(log['visitor_uuid'])
-    users_set=set(users_set)
-    #now that we have list of users we have to calculate reading time spent for each user.
-    #need a dictionary to specify time spent for each user.
-    timeSpent_dict={}
-    for user in users_set:
-        #create a 'time_spent' variable to store the sum of time spent.
-        time_spent=0
-        for log in data['logs']:
-            #check for user event type and uuid to sum up the reading time.
-            if(log['visitor_uuid']==user) and log['event_type']=="pagereadtime":
-                time_spent+=log['event_readtime']
-        #if user has spent time on reading.
-        if time_spent!=0:
-            timeSpent_dict[user]=time_spent
+        timeSpent=0
+        if("event_readtime" in log):
+            timeSpent=log['event_readtime']
+            if log['visitor_uuid'] in users_time.keys():
+                users_time[log['visitor_uuid']]+=timeSpent
+            else:
+                users_time[log['visitor_uuid']]=timeSpent
+
+
     #now that we have the dictionary, let's find the top 10 readers!
     top_readers=[]
     for _ in range(10):
-        maximum = max(timeSpent_dict)
+        maximum = max(users_time)
         top_readers.append(maximum)
-        timeSpent_dict.pop(maximum)
+        users_time.pop(maximum)
     top_readers=set(top_readers)
     print_data(top_readers,'Top 10 Readers','Readers ID')
 
@@ -151,17 +164,22 @@ def also_likes(doc_uuid,user_uuid):
         user_docs = also_likes_user(person)
         for item in user_docs:
             if item!=doc_uuid:
-                docs.append(item)        
+                docs.append(item)   
+    print(docs)     
     repetitons = {}
     for item in docs:
         repetitons[item]=docs.count(item)
 
     
     top_ten=[]
+    print(repetitons)
     for _ in range(10):
-        maximum = max(repetitons)
-        top_ten.append(maximum)
-        repetitons.pop(maximum)
+        if len(repetitons)>0:
+            maximum = max(repetitons)
+            top_ten.append(maximum)
+            repetitons.pop(maximum)
+        else:
+            break
 
     print_data(top_ten, 'Also Likes','Document ID')
 
@@ -316,5 +334,4 @@ exit_btn.place(x=600,y=40)
 
 
 window.mainloop()
-
 #----------------------------------------------------------- GUI -----------------------------------------------------------#
